56f93404902d5f6ec706b4eb85de1daed67a6ab8
Crowdbotics <team@crowdbotics.com>
2022-04-02 01:22:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/samever_test_34570/MainActivity.java
@@ -0,0 +1,15 @@
+package com.samever_test_34570;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "samever_test_34570";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "samever_test_34570",
+  url: "https://samever_test_34570.botics.co",
+  api: "https://samever_test_34570.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
